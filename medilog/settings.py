47469a819666414e2efefb497669ccfc4a36feee
Hajar <hajar@example.com>
2026-05-30 16:08:29 +0000

    Add postgresql support for Render

--- medilog/settings.py
+++ medilog/settings.py
@@ -87,12 +87,17 @@ WSGI_APPLICATION = 'medilog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
